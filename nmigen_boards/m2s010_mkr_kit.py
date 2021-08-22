6b0b55ef796ddc6f50fde3bf0d8f396b6584c6c3
vauban353 <vauban353@gmail.com>
2021-08-22 20:47:13 +0100

    Add support for Digi-Key SmartFusion2 Maker Kit.

---
+++ nmigen_boards/m2s010_mkr_kit.py
@@ -0,0 +1,49 @@
+import subprocess
+
+from nmigen.build import *
+from nmigen.vendor.microchip_smartfusion2 import *
+from .resources import *
+
+
+__all__ = ["DigiKeySmartFusion2MakerKitPlatform"]
+
+
+class DigiKeySmartFusion2MakerKitPlatform(MicrochipSmartFusion2Platform):
+    device      = "M2S010"
+    package     = "144 TQ"
+    speed       = "STD"
+    temperature = "COM"
+    voltage     = "1.2"
+
+    default_clk = "clk_50mhz"
+
+    resources   = [
+        Resource("clk_50mhz", 0, Pins("23", dir="i"),
+                 Clock(50e6), Attrs(GLOBAL=True, IO_STANDARD="LVCMOS33")),
+
+        *LEDResources(pins="129 128 125 124 123 122 118 117", invert=True, attrs=Attrs(IO_STANDARD="LVCMOS33")),
+
+        *SwitchResources(
+            pins="143 144",
+            attrs=Attrs(IO_STANDARD="LVCMOS33")
+        ),
+
+        UARTResource(0,
+            rx="2", tx="1",
+            attrs=Attrs(IO_STANDARD="LVTTL33", PULLUP=1)
+        ),
+
+        *SPIFlashResources(0,
+            cs_n="103", clk="100", copi="102", cipo="101", wp_n="85", hold_n="87",
+            attrs=Attrs(IO_STANDARD="LVCMOS33")
+        ),
+    ]
+
+    connectors = [
+        Connector("mod1", 0, "83 81 90 93 88 82"),
+        Connector("j7", 0, "92 16 94 21 22"),
+    ]
+
+if __name__ == "__main__":
+    from .test.blinky import *
+    DigiKeySmartFusion2MakerKitPlatform().build(Blinky(), do_program=False)
